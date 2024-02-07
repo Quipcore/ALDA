ca2fdd77e40053782d6ca4d174b533f685fdb1b4
Felix Lidö <felix.lido@gmail.com>
2024-02-07 13:19:03 +0100

    Added files for hashtables

---
+++ src/main/java/hashtables/LinearProbingHashTable.java
@@ -0,0 +1,13 @@
+package hashtables;
+
+public class LinearProbingHashTable<T> extends ProbingHashTable<T> {
+
+	/*
+	 * Denna metod ska skrivas klart. Den ska använda linjär sondering och hela tiden öka med ett.
+	 */
+	@Override
+	protected int findPos(T x) {
+		return 0;
+	}
+
+}
